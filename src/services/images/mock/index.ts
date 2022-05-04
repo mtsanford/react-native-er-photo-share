9a0b458781e88ec6f89ecdf87610bf6fe5e4a16b
Mark Sanford <marktsanford@gmail.com>
2022-05-04 11:39:23 -0700

    more mock images

--- src/services/images/mock/index.ts
+++ src/services/images/mock/index.ts
@@ -18,8 +18,216 @@ export const mockAllImages: Record<string, Image> = {
         id: "pic2",
         userId: "user2",
         title: "mock picture 2",
-        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_001_full.jpg?alt=media&token=8ef80f9b-3dbb-450d-b5a1-6b904489308c",
-        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_001_square.jpg?alt=media&token=dd41d1b2-98b5-4d65-8a9c-868a0ba8b0ea",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_002_full.jpg?alt=media&token=22aca420-95a6-4780-bf3f-ca970cee75ef",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_002_square.jpg?alt=media&token=2f5effc6-d527-41ec-bcfa-35026e20d2b3",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic3" : {
+        id: "pic3",
+        userId: "user2",
+        title: "mock picture 3",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_003_full.jpg?alt=media&token=94808dba-2e33-42ae-bb0c-c1f30331943d",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_003_square.jpg?alt=media&token=daddd73a-dba9-458b-b32b-dd1f41463e9f",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic4" : {
+        id: "pic4",
+        userId: "user2",
+        title: "mock picture 4",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_004_full.jpg?alt=media&token=324a9e71-f714-4815-a7d4-de53e9c9edb7",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_004_square.jpg?alt=media&token=3e3ba104-8ae1-4292-b029-d17d5855e266",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic5" : {
+        id: "pic5",
+        userId: "user1",
+        title: "mock picture 5",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_005_full.jpg?alt=media&token=57c179c3-8e98-4190-ba89-b99cecd40c30",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_005_square.jpg?alt=media&token=22cd0102-5c40-4375-be3b-ac32a4ddf962",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic6" : {
+        id: "pic6",
+        userId: "user1",
+        title: "mock picture 6",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_006_full.jpg?alt=media&token=91201f4b-957e-407f-ac62-5cf191fe0948",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_006_square.jpg?alt=media&token=d994ce15-6fae-47e0-beeb-27a9f5e0c5cf",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic7" : {
+        id: "pic7",
+        userId: "user1",
+        title: "mock picture 7",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_007_full.jpg?alt=media&token=92c6b1d8-ccd6-4336-8a40-7323575ac304",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_007_square.jpg?alt=media&token=1d31c82a-9ada-42d4-8f08-554bce362c29",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic8" : {
+        id: "pic8",
+        userId: "user1",
+        title: "mock picture 8",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_008_full.jpg?alt=media&token=e1e22a41-b010-43c8-9a89-bb63e0f3ba30",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_008_square.jpg?alt=media&token=fbaa2d3d-a9cf-4ec0-9e2c-3f0949def1a1",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic9" : {
+        id: "pic9",
+        userId: "user1",
+        title: "mock picture 9",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_009_full.jpg?alt=media&token=7aa74ff9-d8dc-4426-8466-7fd3dd73e5ef",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_009_square.jpg?alt=media&token=14117357-2ac6-460c-8220-f21959bbbde3",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic10" : {
+        id: "pic10",
+        userId: "user1",
+        title: "mock picture 10",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_010_full.jpg?alt=media&token=892c0817-aae5-4fca-afc0-214a1e2da5a7",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_010_square.jpg?alt=media&token=5cbfd016-2578-497e-b318-938fa68a7212",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic11" : {
+        id: "pic11",
+        userId: "user2",
+        title: "mock picture 11",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_011_full.jpg?alt=media&token=d58ad539-e7b5-483c-b7ad-24a10136688d",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_011_square.jpg?alt=media&token=d84784a2-e171-4e44-b666-a09439bbb9bb",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic12" : {
+        id: "pic12",
+        userId: "user2",
+        title: "mock picture 12",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_012_full.jpg?alt=media&token=7740f8bf-6cef-459c-b5a1-132f9b50ceb3",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_012_square.jpg?alt=media&token=76b30ac9-a74d-42c1-bca9-62324a2695c2",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic13" : {
+        id: "pic13",
+        userId: "user2",
+        title: "mock picture 13",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_013_full.jpg?alt=media&token=82283ec8-12cc-4bd2-beee-79981d625a60",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_013_square.jpg?alt=media&token=a18e6384-fb2e-4e37-af0f-ed1d1ffb499e",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic14" : {
+        id: "pic14",
+        userId: "user2",
+        title: "mock picture 14",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_014_full.jpg?alt=media&token=5a3b9698-28c7-44a8-b731-58baf796ad73",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_014_square.jpg?alt=media&token=4a2dba27-14fb-4020-a6e5-2a4895cc8d3a",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic15" : {
+        id: "pic15",
+        userId: "user1",
+        title: "mock picture 15",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_015_full.jpg?alt=media&token=ecfbe26f-b841-4248-a94b-1319735ecdc7",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_015_square.jpg?alt=media&token=7b5cc096-4580-43cd-9193-43d4853bef3d",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic16" : {
+        id: "pic16",
+        userId: "user2",
+        title: "mock picture 16",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_016_full.jpg?alt=media&token=6f5f16da-e5e7-402d-9c12-d782030c9338",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_016_square.jpg?alt=media&token=0cf15129-b060-4893-82eb-5ca8d0721e0f",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic17" : {
+        id: "pic17",
+        userId: "user2",
+        title: "mock picture 17",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_017_full.jpg?alt=media&token=e0acad8b-d8e3-4f3f-9b15-3211536ee43d",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_017_square.jpg?alt=media&token=a19f6e5a-1830-4fc4-9526-1a16d2f08f74",
+        essentialRect: {
+            left: 200,
+            top: 300,
+            width: 600,
+            height: 1200,
+        }
+    },
+    "pic18" : {
+        id: "pic18",
+        userId: "user2",
+        title: "mock picture 18",
+        full: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_018_full.jpg?alt=media&token=898caf86-0c3f-4ed9-a9c6-cb792c0da97a",
+        preview: "https://firebasestorage.googleapis.com/v0/b/er-react-native.appspot.com/o/images%2Fpic_018_square.jpg?alt=media&token=c9197813-056b-4d31-87df-ae74c6a424a0",
         essentialRect: {
             left: 200,
             top: 300,
