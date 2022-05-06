3c200e4115555343113d1008c269288ba9c225eb
Mark Sanford <marktsanford@gmail.com>
2022-05-06 08:33:48 -0700

    flatlist use correct column width

--- src/features/images/recent-images.screen.tsx
+++ src/features/images/recent-images.screen.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { FlatList, TouchableOpacity, Image, View } from "react-native";
+import { FlatList, TouchableOpacity, Image, View, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 
 import { SafeArea } from "../../components/utility/safe-area.component";
@@ -12,12 +12,6 @@ const ImageList = styled(FlatList).attrs({
   },
 })``;
 
-const ImagePreviewWrapper = styled(View)`
-  padding: 3px;
-  flex: 1;
-  aspect-ratio: 1;
-`;
-
 const ImagePreview = styled(Image)`
   width: 100%;
   height: 100%;
@@ -26,11 +20,11 @@ const ImagePreview = styled(Image)`
 const ImageItem = (props) => {
   const { item, navigation } = props;
   return (
-    <ImagePreviewWrapper>
+    <View style={styles.imagePreviewWrapper}>
       <TouchableOpacity onPress={() => navigation.navigate("Details", {item})}>
         <ImagePreview source={{ uri: item.preview }} />
       </TouchableOpacity>
-    </ImagePreviewWrapper>
+    </View>
   );
 };
 
@@ -49,3 +43,12 @@ export const RecentImagesScreen = ({ navigation }) => {
     </SafeArea>
   );
 };
+
+
+const styles = StyleSheet.create({
+  imagePreviewWrapper: {
+    padding: 2,
+    flex: 1 / 2,
+    aspectRatio: 1,
+  },
+})
