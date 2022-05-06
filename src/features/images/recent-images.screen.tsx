dc48eef4fe06e52d570e1772266426305725f562
Mark Sanford <marktsanford@gmail.com>
2022-05-06 16:31:49 -0700

    2-5 cols for ilst

--- src/features/images/recent-images.screen.tsx
+++ src/features/images/recent-images.screen.tsx
@@ -1,5 +1,11 @@
-import React, { useContext } from "react";
-import { FlatList, TouchableOpacity, Image, View, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import {
+  FlatList,
+  TouchableOpacity,
+  Image,
+  View,
+  StyleSheet,
+} from "react-native";
 import styled from "styled-components/native";
 
 import { SafeArea } from "../../components/utility/safe-area.component";
@@ -12,16 +18,87 @@ const ImageList = styled(FlatList).attrs({
   },
 })``;
 
+const ImageList2Col = ({ images, navigation }) => (
+  <ImageList
+    data={images}
+    numColumns={2}
+    renderItem={({ item }) => (
+      <ImageItem
+        item={item}
+        navigation={navigation}
+        style={styles.imagePreviewWrapper_2_column}
+      />
+    )}
+    keyExtractor={(item) => item.id + '_2'}
+  />
+);
+
+const ImageList3Col = ({ images, navigation }) => (
+  <ImageList
+    data={images}
+    numColumns={3}
+    renderItem={({ item }) => (
+      <ImageItem
+        item={item}
+        navigation={navigation}
+        style={styles.imagePreviewWrapper_3_column}
+      />
+    )}
+    keyExtractor={(item) => item.id + '_3'}
+  />
+);
+
+const ImageList4Col = ({ images, navigation }) => (
+  <ImageList
+    data={images}
+    numColumns={4}
+    renderItem={({ item }) => (
+      <ImageItem
+        item={item}
+        navigation={navigation}
+        style={styles.imagePreviewWrapper_4_column}
+      />
+    )}
+    keyExtractor={(item) => item.id + '_4'}
+  />
+);
+
+const ImageList5Col = ({ images, navigation }) => (
+  <ImageList
+    data={images}
+    numColumns={5}
+    renderItem={({ item }) => (
+      <ImageItem
+        item={item}
+        navigation={navigation}
+        style={styles.imagePreviewWrapper_5_column}
+      />
+    )}
+    keyExtractor={(item) => item.id + '_5'}
+  />
+);
+
 const ImagePreview = styled(Image)`
   width: 100%;
   height: 100%;
 `;
 
+const calcColumns = (width: number, height: number) => {
+  const aspectRatio = width / height;
+  console.log(aspectRatio);
+  if (aspectRatio > 1.9) return 5;
+  if (aspectRatio > 1.4) return 4;
+  if (aspectRatio > 0.9) return 3;
+  return 2;
+};
+
 const ImageItem = (props) => {
-  const { item, navigation } = props;
+  const { item, navigation, style } = props;
   return (
-    <View style={styles.imagePreviewWrapper}>
-      <TouchableOpacity onPress={() => navigation.navigate("Details", {item})}>
+    <View style={style}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Details", { item })}
+      >
         <ImagePreview source={{ uri: item.preview }} />
       </TouchableOpacity>
     </View>
@@ -30,25 +107,44 @@ const ImageItem = (props) => {
 
 export const RecentImagesScreen = ({ navigation }) => {
   const { recentImages } = useContext(ImagesContext);
+  const [ numColumns, setNumColumns ] = useState(0);
+
+  const onLayout = (event) => {
+    var { width, height } = event.nativeEvent.layout;
+    setNumColumns(calcColumns(width, height));
+  };
+
   return (
     <SafeArea>
-      <ImageList
-        data={recentImages}
-        numColumns={2}
-        renderItem={({ item }) => (
-          <ImageItem item={item} navigation={navigation} />
-        )}
-        keyExtractor={(item) => item.id}
-      ></ImageList>
+      <View onLayout={onLayout}>
+        {(numColumns == 2) && <ImageList2Col images={recentImages} navigation={navigation} />}
+        {(numColumns == 3) && <ImageList3Col images={recentImages} navigation={navigation} />}
+        {(numColumns == 4) && <ImageList4Col images={recentImages} navigation={navigation} />}
+        {(numColumns == 5) && <ImageList5Col images={recentImages} navigation={navigation} />}
+      </View>
     </SafeArea>
   );
 };
 
-
 const styles = StyleSheet.create({
-  imagePreviewWrapper: {
+  imagePreviewWrapper_2_column: {
     padding: 2,
     flex: 1 / 2,
     aspectRatio: 1,
   },
-})
+  imagePreviewWrapper_3_column: {
+    padding: 2,
+    flex: 1 / 3,
+    aspectRatio: 1,
+  },
+  imagePreviewWrapper_4_column: {
+    padding: 2,
+    flex: 1 / 4,
+    aspectRatio: 1,
+  },
+  imagePreviewWrapper_5_column: {
+    padding: 2,
+    flex: 1 / 5,
+    aspectRatio: 1,
+  },
+});
