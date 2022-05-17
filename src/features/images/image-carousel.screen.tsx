154816e96503197a61c822e6a85576ffa0fe4c0b
Mark Sanford <marktsanford@gmail.com>
2022-05-17 16:46:44 -0700

    try separate flatlist for each orientation

--- src/features/images/image-carousel.screen.tsx
+++ src/features/images/image-carousel.screen.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useState, useEffect, useCallback, useRef } from "react";
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+} from "react";
 import { Dimensions } from "react-native";
 import { FlatList, View, StyleSheet } from "react-native";
 
@@ -9,79 +15,104 @@ import { ImagesContext } from "../../services/images/images.context";
 
 const ImageItem = ({ item, screenDimensions }) => {
   console.log(`ImageItem render...width ${screenDimensions.width}`);
-  const containerStyle = { width: screenDimensions.width, height: screenDimensions.height };
+  const containerStyle = {
+    width: screenDimensions.width,
+    height: screenDimensions.height,
+  };
   return (
     <View style={containerStyle}>
-      <EssentialRectImage
-        src={item.full}
-        essentialRect={item.essentialRect}
-      />
+      <EssentialRectImage src={item.full} essentialRect={item.essentialRect} />
     </View>
   );
-}
+};
 
-const imageItemPropsAreEqual = (prev, next) => (
+const imageItemPropsAreEqual = (prev, next) =>
   prev.item.full === next.item.full &&
   prev.screenDimensions.width === next.screenDimensions.width &&
-  prev.screenDimensions.height === next.screenDimensions.height
-);
+  prev.screenDimensions.height === next.screenDimensions.height;
 
-const MemoedImageItem = React.memo(ImageItem, imageItemPropsAreEqual)
+const MemoedImageItem = React.memo(ImageItem, imageItemPropsAreEqual);
 
 /************/
 
-export const ImageCarouselScreen = ({ navigation }) => {
-  const [screenDimensions, setScreenDimensions] = useState(Dimensions.get("screen"));
-  const [imageIndex, setImageIndex] = useState(0);
-  const { recentImages } = useContext(ImagesContext);
-  const ref = useRef<FlatList>();
+const ImageCarouselList = ({ imageList, screenDimensions, initialIndex, onIndexChanged }) => {
 
   const { height: itemSize } = screenDimensions;
 
-  const onViewableItemsChanged = useCallback( ({viewableItems}) => {
+  const onViewableItemsChanged = useCallback(({ viewableItems }) => {
     if (viewableItems.length === 1) {
-      setImageIndex(viewableItems[0].index);
+      onIndexChanged(viewableItems[0].index);
     }
-  }, []);
+  }, [onIndexChanged]);
 
-  const getItemLayout = useCallback( (_, index) => ({
-    length: itemSize,
-    offset: itemSize * index,
-    index,
-  }), [itemSize]);
-
-  useEffect( () => {
-    const screenChangedHandler = ({screen}) => {
-      // we're getting a new object, so to prevent excessive rerenders, to deep check for equality
-      setScreenDimensions( prevScreen => (prevScreen.width === screen.width && prevScreen.height === screen.height) ? prevScreen : screen );
-    }
-
-    const sub = Dimensions.addEventListener("change", screenChangedHandler);
-    return () => Dimensions.removeEventListener("change", screenChangedHandler);
-  }, [])
-
-  useEffect( () => {
-    if (imageIndex) {
-      ref.current?.scrollToIndex({ animated: false, index: imageIndex });
-    }
-  }, [screenDimensions])
+  const getItemLayout = useCallback(
+    (_, index) => ({
+      length: itemSize,
+      offset: itemSize * index,
+      index,
+    }),
+    [itemSize]
+  );
 
   return (
-    <View style={styles.carousel}>
       <FlatList
-        ref={ref}
-        data={recentImages}
-        renderItem={ ({item}) => <MemoedImageItem item={item} screenDimensions={screenDimensions} /> }
+        data={imageList}
+        renderItem={({ item }) => (
+          <MemoedImageItem item={item} screenDimensions={screenDimensions} />
+        )}
         onViewableItemsChanged={onViewableItemsChanged}
         snapToAlignment="start"
         decelerationRate={"fast"}
         snapToInterval={itemSize}
         getItemLayout={getItemLayout}
         keyExtractor={(item) => item.id}
+        initialScrollIndex={initialIndex}
         initialNumToRender={1}
         maxToRenderPerBatch={2}
         windowSize={5}
       />
+  );
+};
+
+type Orientation = 'portrait' | 'landscape';
+
+export const ImageCarouselScreen = ({ navigation }) => {
+  const [screenDimensions, setScreenDimensions] = useState(Dimensions.get("screen"));
+  const [imageIndex, setImageIndex] = useState(0);
+  const { recentImages } = useContext(ImagesContext);
+  // const ref = useRef<FlatList>();
+
+  const orientation = screenDimensions.width > screenDimensions.height ? 'landscape' : 'portrait';
+  const { height: itemSize } = screenDimensions;
+
+  const onIndexChanged = useCallback((index) => {
+    setImageIndex(index);
+  }, []);
+
+  useEffect(() => {
+    const screenChangedHandler = ({ screen }) => {
+      // we're getting a new object, so to prevent excessive rerenders, to deep check for equality
+      setScreenDimensions((prevScreen) =>
+        prevScreen.width === screen.width && prevScreen.height === screen.height
+          ? prevScreen
+          : screen
+      );
+    };
+
+    const sub = Dimensions.addEventListener("change", screenChangedHandler);
+    return () => Dimensions.removeEventListener("change", screenChangedHandler);
+  }, []);
+
+  // useEffect(() => {
+  //   if (imageIndex) {
+  //     ref.current?.scrollToIndex({ animated: false, index: imageIndex });
+  //   }
+  // }, [screenDimensions]);
+
+  return (
+    <View style={styles.carousel}>
+      {(orientation === 'landscape') && <ImageCarouselList imageList={recentImages} screenDimensions={screenDimensions} initialIndex={imageIndex} onIndexChanged={onIndexChanged} />}
+      {(orientation === 'portrait') && <ImageCarouselList imageList={recentImages} screenDimensions={screenDimensions} initialIndex={imageIndex} onIndexChanged={onIndexChanged} />}
     </View>
   );
 };
