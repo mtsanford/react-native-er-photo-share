b6301967bd219ed79db60dfaf627fdde518655e4
Mark Sanford <marktsanford@gmail.com>
2022-07-15 07:36:25 -0700

    try refactoring ImageCarouselScreen as one component

--- src/features/images/image-carousel.screen.tsx
+++ src/features/images/image-carousel.screen.tsx
@@ -35,17 +35,27 @@ const MemoedImageItem = React.memo(ImageItem, imageItemPropsAreEqual);
 
 /************/
 
-const ImageCarouselList = ({ imageList, screenDimensions, initialIndex, onIndexChanged }) => {
+const ImageCarouselList = ({
+  imageList,
+  screenDimensions,
+  initialIndex,
+  onIndexChanged,
+}) => {
   const { height: itemSize } = screenDimensions;
 
-  console.log(`ImageCarouselList initialIndex=${initialIndex} itemSize=${itemSize}`);
-  
-  const onViewableItemsChanged = useCallback(({ viewableItems }) => {
-    if (viewableItems.length === 1) {
-      console.log("onViewableItemsChanged changed ", viewableItems[0].index);
-      onIndexChanged(viewableItems[0].index);
-    }
-  }, [onIndexChanged]);
+  console.log(
+    `ImageCarouselList initialIndex=${initialIndex} itemSize=${itemSize}`
+  );
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }) => {
+      if (viewableItems.length === 1) {
+        console.log("onViewableItemsChanged changed ", viewableItems[0].index);
+        onIndexChanged(viewableItems[0].index);
+      }
+    },
+    [onIndexChanged]
+  );
 
   const getItemLayout = useCallback(
     (_, index) => ({
@@ -57,44 +67,59 @@ const ImageCarouselList = ({ imageList, screenDimensions, initialIndex, onIndexC
   );
 
   return (
-      <FlatList
-        data={imageList}
-        renderItem={({ item }) => (
-          <MemoedImageItem item={item} screenDimensions={screenDimensions} />
-        )}
-        onViewableItemsChanged={onViewableItemsChanged}
-        snapToAlignment="start"
-        decelerationRate={"fast"}
-        snapToInterval={itemSize}
-        getItemLayout={getItemLayout}
-        keyExtractor={(item) => item.id}
-        initialScrollIndex={initialIndex}
-        initialNumToRender={1}
-        maxToRenderPerBatch={2}
-        windowSize={5}
-      />
+    <FlatList
+      data={imageList}
+      renderItem={({ item }) => (
+        <MemoedImageItem item={item} screenDimensions={screenDimensions} />
+      )}
+      onViewableItemsChanged={onViewableItemsChanged}
+      snapToAlignment="start"
+      decelerationRate={"fast"}
+      snapToInterval={itemSize}
+      getItemLayout={getItemLayout}
+      keyExtractor={(item) => item.id}
+      initialScrollIndex={initialIndex}
+      initialNumToRender={1}
+      maxToRenderPerBatch={2}
+      windowSize={5}
+    />
   );
 };
 
-type Orientation = 'portrait' | 'landscape';
+type Orientation = "portrait" | "landscape";
 
 export const ImageCarouselScreen = ({ navigation }) => {
-  const [screenDimensions, setScreenDimensions] = useState(Dimensions.get("screen"));
+  const [screenDimensions, setScreenDimensions] = useState(
+    Dimensions.get("screen")
+  );
   // const [imageIndex, setImageIndex] = useState(0);
   const { recentImages } = useContext(ImagesContext);
-  const indexRef = useRef<Number>(0);
-  // const ref = useRef<FlatList>();
+  const indexRef = useRef<number>(0);
+  const listRef = useRef<FlatList>();
 
-  const orientation = screenDimensions.width > screenDimensions.height ? 'landscape' : 'portrait';
+  // const orientation =
+  //   screenDimensions.width > screenDimensions.height ? "landscape" : "portrait";
+
   const { height: itemSize } = screenDimensions;
 
-  console.log("ImageCarouselScreen")
+  console.log(`ImageCarouselScreen itemSize=${itemSize}`);
 
-  const onIndexChanged = useCallback((index) => {
-    console.log('new image index = ', index)
-    indexRef.current = index;
-    //setImageIndex(index);
-  }, []);
+  // const onIndexChanged = useCallback((index) => {
+  //   console.log("new image index = ", index);
+  //   indexRef.current = index;
+  //   //setImageIndex(index);
+  // }, []);
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }) => {
+      if (viewableItems.length === 1) {
+        console.log("onViewableItemsChanged changed ", viewableItems[0].index);
+        // onIndexChanged(viewableItems[0].index);
+        indexRef.current = viewableItems[0].index;
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     const screenChangedHandler = ({ screen }) => {
@@ -112,19 +137,41 @@ export const ImageCarouselScreen = ({ navigation }) => {
 
   useEffect(() => {
     return () => {
-      console.log('ImageCarouselScreen unloaded')
-    }
-  }, [])
+      console.log("ImageCarouselScreen unloaded");
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   if (imageIndex) {
-  //     ref.current?.scrollToIndex({ animated: false, index: imageIndex });
-  //   }
-  // }, [screenDimensions]);
+  useEffect(() => {
+    console.log('scrollToIndex ', indexRef.current);
+    listRef.current?.scrollToIndex({ animated: false, index: indexRef.current });
+  }, [screenDimensions]);
+
+  const getItemLayout = (_, index) => ({
+    length: itemSize,
+    offset: itemSize * index,
+    index,
+  });
 
   return (
     <View style={styles.carousel}>
-      <ImageCarouselList imageList={recentImages} screenDimensions={screenDimensions} initialIndex={indexRef.current} onIndexChanged={onIndexChanged} />
+      <FlatList
+        data={recentImages}
+        renderItem={({ item }) => (
+          <MemoedImageItem item={item} screenDimensions={screenDimensions} />
+        )}
+        onViewableItemsChanged={onViewableItemsChanged}
+        snapToAlignment="start"
+        decelerationRate={"fast"}
+        snapToInterval={itemSize}
+        getItemLayout={getItemLayout}
+        keyExtractor={(item) => item.id}
+        // initialScrollIndex={indexRef.current}
+        initialNumToRender={1}
+        maxToRenderPerBatch={2}
+        windowSize={5}
+        ref={listRef}
+      />
+      {/* <ImageCarouselList imageList={recentImages} screenDimensions={screenDimensions} initialIndex={indexRef.current} onIndexChanged={onIndexChanged} /> */}
     </View>
   );
 };
