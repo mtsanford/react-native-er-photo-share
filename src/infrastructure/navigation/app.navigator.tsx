611cd7fe46b8c09ee254bccfd930a8b163385b55
Mark Sanford <marktsanford@gmail.com>
2022-07-22 09:30:42 -0700

    Reorganized navigation so carousel is modal

--- src/infrastructure/navigation/app.navigator.tsx
+++ src/infrastructure/navigation/app.navigator.tsx
@@ -1,64 +1,30 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { PicturesNavigator } from "./pictures.navigator";
-import { PostScreen } from "../../features/post/post.screen";
-import { ProfileScreen } from "../../features/profile/profile.screen";
+import { HomeNavigator } from "./home.navigator";
+import { ImageCarouselScreen } from "../../features/images/image-carousel.screen";
 
-const Tab = createBottomTabNavigator();
+const RootStack = createStackNavigator();
 
-const TAB_ICON = {
-  Pictures: "image",
-  Post: "add-circle-outline",
-  Profile: "person-outline",
+export const AppNavigator = () => {
+  return (
+    <RootStack.Navigator>
+      <RootStack.Group>
+        <RootStack.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <RootStack.Screen
+          name="Carousel"
+          component={ImageCarouselScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </RootStack.Group>
+    </RootStack.Navigator>
+  );
 };
-
-// const createScreenOptions = ({ route }) => {
-//   const iconName = TAB_ICON[route.name];
-//   return {
-//     tabBarIcon: ({ size, color }) => (
-//       <Ionicons name={iconName} size={size} color={color} />
-//     ),
-//   };
-// };
-
-export const AppNavigator = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarActiveTintColor: "tomato",
-      tabBarInactiveTintColor: "gray",
-    }}
-  >
-    <Tab.Screen
-      options={{
-        tabBarLabel: "Pictures",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="image" color={color} size={size} />
-        ),
-      }}
-      name="Pictures"
-      component={PicturesNavigator}
-    />
-    <Tab.Screen
-      options={{
-        tabBarLabel: "Post",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="add-circle-outline" color={color} size={size} />
-        ),
-      }}
-      name="Post"
-      component={PostScreen}
-    />
-    <Tab.Screen
-      options={{
-        tabBarLabel: "Profile",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="person-outline" color={color} size={size} />
-        ),
-      }}
-      name="Profile"
-      component={ProfileScreen}
-    />
-  </Tab.Navigator>
-);
---
+++ src/infrastructure/navigation/home.navigator.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { RecentImagesScreen } from "../../features/images/recent-images.screen"
+import { PostScreen } from "../../features/post/post.screen";
+import { ProfileScreen } from "../../features/profile/profile.screen";
+
+const Tab = createBottomTabNavigator();
+
+export const HomeNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: "tomato",
+      tabBarInactiveTintColor: "gray",
+    }}
+  >
+    <Tab.Screen
+      options={{
+        tabBarLabel: "Pictures",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="image" color={color} size={size} />
+        ),
+      }}
+      name="Pictures"
+      component={RecentImagesScreen}
+    />
+    <Tab.Screen
+      options={{
+        tabBarLabel: "Post",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="add-circle-outline" color={color} size={size} />
+        ),
+      }}
+      name="Post"
+      component={PostScreen}
+    />
+    <Tab.Screen
+      options={{
+        tabBarLabel: "Profile",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person-outline" color={color} size={size} />
+        ),
+      }}
+      name="Profile"
+      component={ProfileScreen}
+    />
+  </Tab.Navigator>
+);
