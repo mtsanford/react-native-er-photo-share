565101205a4f34ccb99325d51d50ca272aede446
Mark Sanford <marktsanford@gmail.com>
2022-07-23 10:53:49 -0700

    PostScreen.. use ImagePicker to select image

--- src/features/post/post.screen.tsx
+++ src/features/post/post.screen.tsx
@@ -1,16 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform, Text } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import styled from "styled-components/native";
-import { View, Text } from "react-native";
 
 export const PostPlaceHolder = styled(View)`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const PostScreen = () => {
-    return (
-        <PostPlaceHolder>
-            <Text>POST IMAGE</Text>
-        </PostPlaceHolder>
-    );
-}
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <PostPlaceHolder>
+      <Button title="Take Picture" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
+    </PostPlaceHolder>
+  );
+};
